ff7e355e1eab7b219d05c5809f9d9b85797a519d
kragleh <danidurisik@gmail.com>
2024-10-03 12:03:23 +0200

    Koniec Hodiny

--- 3.10.24/zahrada.php
+++ 3.10.24/zahrada.php
@@ -1,7 +1,11 @@
 <?php
   $a=20;
   $b=5;
-  $c=$a*2+$b*2;
+  // $c=$a*2+$b*2;
 
-  echo "Na oplotenie zahrady s rozmermi $a x $b potrebujeme $c m pletiva.";
+  echo "Na oplotenie zahrady s rozmermi $a x $b potrebujeme " .(2*($a+$b)). " m pletiva.<br>";
+
+  echo "Vygenerovane: " .date("d.m.Y H:i:s"). "<br>";
+
+  echo "Today is: " .date("l, d.m.Y");
 ?>
